69c6b5d1beba17afd8363613ea25bfa7675c196e
Andika Ahmad Ramadhan <aspendaka@gmail.com>
2018-04-11 11:28:24 +0700

    Bug fixing dependencies

---
+++ src/peserta.js
@@ -0,0 +1,24 @@
+var peserta = [{
+    no : '001',
+    name : 'Andika Ahmad Ramadhan'
+},
+{
+    no : '002',
+    name : 'Arbi'
+},
+{
+    no : '003',
+    name : 'Fandi'
+},
+{
+    no : '003',
+    name : 'Ojik'
+},
+{
+    no : '005',
+    name : 'Fandi'
+},
+{
+    no : '006',
+    name : 'Varen'
+}];
